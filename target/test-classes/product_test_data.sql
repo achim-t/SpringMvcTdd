4e975060ad300934425efc5824c4fa3aa3849bd0
Achim Taeschner <achim.taeschner@ds-experts.de>
2015-10-29 13:06:00 +0100

    delete and ignore

--- target/test-classes/product_test_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO product(name, description) VALUES ('Very Nice Shoes','Very Nice Shoes made in Thailand')
-INSERT INTO product(name, description) VALUES ('Cool Shoes','Cool Shoes made in Japan')
